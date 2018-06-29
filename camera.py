16af3f632b42e6614e08aef4c1153cd6a4d96de7
David Abraham <david.abraham.1336@gmail.com>
2018-06-29 13:21:54 -0700

    Add lerp to camera follow

--- camera.py
+++ camera.py
@@ -3,9 +3,10 @@ import pygame
 from entity import Entity
 
 class Camera(Entity):
-    def __init__(self, size):
+    def __init__(self, size, lerp_speed=5):
         Entity.__init__(self, pygame.math.Vector2(0, 0), 0, pygame.math.Vector2(1, 1))
         self.entity_to_follow = None
+        self.lerp_speed = lerp_speed
 
     def is_visible(self, entity):
         pass
@@ -15,6 +16,6 @@ class Camera(Entity):
     
     def update(self, dt):
         if self.entity_to_follow is not None:
-            self.position = self.entity_to_follow.get_position()
+            self.position = self.position.lerp(self.entity_to_follow.get_position(), dt * self.lerp_speed)
 
 main = Camera((200, 200))
